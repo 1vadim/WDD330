046ac6c0eaf8ae6b37f41e0698ec5af88a14c1a6
1vadim <vchemariev@byupathway.edu>
2026-05-27 14:16:35 +0200

    w3-indTask-searchBox

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -11,18 +11,10 @@ function convertToJson(res) {
 export default class ProductData {
   constructor() {}
   async getData(category) {
-    const response = await fetch(`${baseURL}products/search/${category} `);
+    const response = await fetch(`${baseURL}products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
   }
-
-  // async getData() {
-  //   const response = await fetch(this.path);
-  //   return await convertToJson(response);
-  // }
-  // async findProductById(id) {
-  //   const products = await this.getData();
-  //   return products.find((item) => item.Id === id);
 
   async findProductById(id) {
     const response = await fetch(`${baseURL}product/${id}`);
